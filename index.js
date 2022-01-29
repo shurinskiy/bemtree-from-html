98189cfa5226a7ed6e22dbdefb9c04359ef4c202
_tango <shurinskiy@mail.ru>
2022-01-29 17:14:34 +0600

    add data- attribute and 'use' property

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const path = require('path');
 const glob = require('glob');
 const pckg = require(path.join(process.cwd(), 'package.json'));
 
-let jsImportPath = '';
 let options = {
 	cwd: process.cwd(),
 	from:'src/**/*.html',
 	to: 'src/blocks',
-	js: false,
-	omit: false
+	omit: '@@',
+	use: '',
+	js: ''
 };
 
 if (pckg.bemtree)
 	options = {...options, ...pckg.bemtree }
 
-if (options.js)
-	jsImportPath = path.posix.relative(path.dirname(options.js), options.to);
-
 
 /* Создать массив из всех классов которые есть в файлах найденных по заданному шаблону поиска */
 const getClasses = (options) => {
-	let classes = [];
 	let html = '';
+	let entries = {
+		classes: [],
+		attrs: []
+	};
 
 	glob.sync(options.from.replace('%', '|'), {cwd:options.cwd}).forEach((file) => {
 		try {
@@ -33,16 +33,26 @@ const getClasses = (options) => {
 			console.error(err)
 		}
 	
-		let classMatches = html.match(/class=("([^"]*)")|class=('([^']*)')/ig) || [];
+		let classMatches = html.match(/class=("([^"]*)")|class=('([^']*)')/ig) || []; // классы
+		let attrMatches = html.match(/\bdata-(-|\w)+/ig) || []; // data- атрибуты
 		
 		classMatches.forEach((classString) => {
 			let classesFromString = classString.replace(/class=/i, '').replace(/'|"/g, '').match(/\S+/g) || [];
-			classes = [...classes, ...classesFromString];
-		})
+			entries.classes = [...entries.classes, ...classesFromString];
+		});
+
+		attrMatches.forEach((attrString) => {
+			let attrsFromString = attrString.replace(/data-/i, '').match(/\S+/g) || [];
+			entries.attrs = [...entries.attrs, ...attrsFromString];
+		});
+
 	});
 
+	// удаляю из массива повторяющиеся имена data- атрибутов
+	entries.attrs = Array.from(new Set(entries.attrs));
+	
 	// удаляю из массива повторяющиеся имена классов, а уникальные сортирую по блокам
-	classes = Array.from(new Set(classes)).sort((a, b) => {
+	entries.classes = Array.from(new Set(entries.classes)).sort((a, b) => {
 		a = a.split('_')[0];
 		b = b.split('_')[0];
 		return (a < b) ? -1 : (a > b) ? 1 : 0;
@@ -51,41 +61,46 @@ const getClasses = (options) => {
 		return ((a.indexOf('_') == -1) && (a == b.split('_')[0])) ? -1 : 0;
 	});
 
-	return classes;
+
+	return entries;
 }
 
 
 /* Преобразовать массив классов в объект с вложенностью соответствующей БЭМ иерархии */
-const toJSON = (classes, options) => {
+const toJSON = (entries, options) => {
 	let bemjson = {};
 	let blockname = '';
-	let omit = options.omit ? new RegExp(`^(${options.omit.replace(/\s*/g, '').replace(/,/g, '|')}).*?`) : false;
+	let omit = new RegExp(`^(${options.omit.replace(/\s*/g, '').replace(/,/g, '|')}).*?`);
+	let use = new RegExp(`^(${options.use.replace(/\s*/g, '').replace(/,/g, '|')}).*?`);
 
 	// наполняю bemjson
-	classes.forEach((item) => {
+	entries.classes.forEach((item) => {
 		// если имя класса есть в списке исключений - пропускаю
-		if(item.match(omit)) return;
+		if(!item.match(use) || item.match(omit)) return;
 
 		// если нет _ - блок
 		if ((item.indexOf('_') == -1) && item.indexOf('-js') == -1) {
 			blockname = item;
 			bemjson[blockname] = {'mods':'','elems':{}};
 		}
+
 		// если есть блок-js - создать js файл с именем блока
-		if ((item.indexOf(blockname + '-js') > -1)) {
+		if ((entries.attrs.indexOf(blockname) > -1) || (item.indexOf(blockname + '-js') > -1)) {
 			bemjson[blockname]['js'] = true;
 		}
+
 		// если только один _ - модификатор блока
 		if ((item.match(/_/g) || []).length == 1) {
 			item = item.replace(blockname+'_', '');
 			bemjson[blockname]['mods'] += (item + ',');
 		}
+
 		// если есть blockname__ - элемент или его модификатор
 		if ((item.indexOf(blockname+'__') > -1)) {
 			let rest = item.replace(blockname+'__', '').split('_');
 			let elem = bemjson[blockname]['elems'][rest[0]];
 
-			if (! elem)
+			if (!elem)
 				bemjson[blockname]['elems'][rest[0]] = {'mods':''};
 
 			if(rest[1])
@@ -99,6 +114,7 @@ const toJSON = (classes, options) => {
 
 /* Создать файловую структуру БЭМ для SCSS в заданной директории */
 const createStucture = (bemjson, options) => {
+	let jsImportPath = path.posix.relative(path.dirname(options.js), options.to);
 	let imports = [];
 
 	for (const blockName in bemjson) {
@@ -148,11 +164,12 @@ const createStucture = (bemjson, options) => {
 		createFiles(filePath, fileContent);
 	}
 
-	if(options.js && imports.length)
+	if(options.js && imports.length) {
 		addImportJS(options, imports);
+	}
 }
-
-
+	
+	
 const fileExist = (filePath) => {
 	try {
 		fs.statSync(filePath);
@@ -172,11 +189,11 @@ const createFiles = (filePath, fileContent) => {
 		});
 	}
 }
-
-
+	
+	
 const addImportJS = (options, imports) => {
-	const lb = require('os').EOL;
-	let content = [];
+		const lb = require('os').EOL;
+		let content = [];
 
 	if (fileExist(options.js) === false) {
 		try {
